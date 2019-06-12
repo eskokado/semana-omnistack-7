6d087047f39357a0c5e4f19fc1f09633983f8240
Gabriel Monteiro Fernandes <mfgbriel92@gmail.com>
2019-06-12 13:16:38 -0300

    Quarta - acesso à API com axios

--- webroot/src/pages/Feed.js
+++ webroot/src/pages/Feed.js
@@ -3,41 +3,57 @@ import more from '../assets/more.svg'
 import like from '../assets/like.svg'
 import comment from '../assets/comment.svg'
 import send from '../assets/send.svg'
+import api from '../services/api'
 import '../styles/Feed.css'
 
 class Feed extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      posts: []
+    }
+  }
+
+  async componentDidMount () {
+    const response = await api.get('posts')
+    this.setState({ posts: response.data })
+  }
+
   render () {
     return (
       <section id="posts">
-        <article>
-          <header>
-            <div className="user-info">
-              <span className="user-name">Gabriel Fernandes</span>
-              <span className="user-location">Novo Hamburgo, RS</span>
-            </div>
+        {this.state.posts.map(post => (
+          <article key={post._id}>
+            <header>
+              <div className="user-info">
+                <span className="user-name">{post.author}</span>
+                <span className="user-location">{post.location}</span>
+              </div>
 
-            <img src={more} alt="More..." />
-          </header>
+              <img src={more} alt="More..." />
+            </header>
 
-          <img className="post-image" src="https://picsum.photos/500/500" alt=""/>
+            <img className="post-image" src="https://picsum.photos/500/500" alt="" />
 
-          <footer>
-            <div className="actions">
-              <img src={like} alt="More..." />
-              <img src={comment} alt="More..." />
-              <img src={send} alt="More..." />
-            </div>
+            <footer>
+              <div className="actions">
+                <img src={like} alt="More..." />
+                <img src={comment} alt="More..." />
+                <img src={send} alt="More..." />
+              </div>
 
-            <strong>900 likes</strong>
+              <strong>{post.likes} likes</strong>
 
-            <p className="post-description">
-              Lorem ipsum dolor sit amet
-              <span className="post-hashtags">
-                #foo #bar #lorem
-              </span>
-            </p>
-          </footer>
-        </article>
+              <p className="post-description">
+                {post.legend}
+                <span className="post-hashtags">
+                  {post.hashtags}
+                </span>
+              </p>
+            </footer>
+          </article>
+        ))}
       </section>
     )
   }
